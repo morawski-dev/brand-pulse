a19cde73e7bede0aee606489b4fe068f3c4ae4d6
Mateusz Morawski <35307739+morawski-dev@users.noreply.github.com>
2025-11-28 23:23:27 +0100

    update BackendApplicationTests

--- backend/src/test/java/com/morawski/dev/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/morawski/dev/backend/BackendApplicationTests.java
@@ -1,9 +1,13 @@
 package com.morawski.dev.backend;
 
+import com.morawski.dev.backend.config.OpenRouterConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, OpenRouterConfig.class})
 class BackendApplicationTests {
 
     @Test
